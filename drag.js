cce39fd61747a245ceae1c4a169f603c3454ee76
laibao <369632567@qq.com>
2016-05-04 23:20:21 +0800

    "23.20 2016.5.4 last submit"

--- drag.js
+++ drag.js
@@ -1,7 +1,12 @@
 
 function(id){
 	var obj = document.getElementById(id);
-	obj.onmousedown=function(){
+	var disX=0;
+	var disY=0;
+	obj.onmousedown=function(ev){
+		var ev = ev || event;
+		disX=ev.clientX;
+		disY=ev.clientY;
 		
 	}
 	
