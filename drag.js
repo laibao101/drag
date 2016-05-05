7f69c99beda20bd2569bbc777f7bc5912adfc96a
laibao <369632567@qq.com>
2016-05-05 17:44:04 +0800

    drag success

--- drag.js
+++ drag.js
@@ -1,18 +1,39 @@
 
-function(id){
+function drag(id){
 	var obj = document.getElementById(id);
 	var disX=0;
 	var disY=0;
 	obj.onmousedown=function(ev){
 		var ev = ev || event;
-		disX=ev.clientX;
-		disY=ev.clientY;
-		obj.onmousemove=function(){
+		disX=ev.clientX - obj.offsetLeft;
+		disY=ev.clientY - obj.offsetTop;
+		console.log( disX+',' +disY);
+		document.onmousemove=function(ev){
+			var ev =ev || event;
+			var L=ev.clientX - disX;
+			var T=ev.clientY - disY;
+			if( L<0){
+				L=0;
+			}
+			else if(L > document.documentElement.offsetWidth - obj.offsetWidth) {
+				L = document.documentElement.offsetWidth - obj.offsetWidth;
+			}
+			if(T<0){
+				T=0;
+			}
+			else if(T>document.documentElement.clientHeight - obj.offsetHeight){
+				T=document.documentElement.clientHeight - obj.offsetHeight;
+			}
+			obj.style.left= L + 'px';
+			obj.style.top= T + 'px';
+			
 			
 		}
-		obj.onnmouseup=function(){
-			obj.onmousemove=null;
+		document.onmouseup=function(){
+			document.onmousemove=null;
+			document.onmouseup=null;
 		}
+		return false;
 	}
 	
 }
